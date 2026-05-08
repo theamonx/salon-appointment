996b9de8d50995af1c1ffc9095dbf3f4414b125a
theamonx <codeamadev@gmail.com>
2026-05-08 23:20:16 +0530

    fixed responsive navlink

--- _nuxt/DqpWjJRv.js
+++
@@ -1,1 +0,0 @@
-import"./B7bUGqWu.js";const e=""+new URL("salon.D8ktiJZs.jpg",import.meta.url).href;export{e as i};
---
+++ _nuxt/Y4rSkc_w.js
@@ -0,0 +1,1 @@
+import"./DvNVjVU-.js";const e=""+new URL("salon.D8ktiJZs.jpg",import.meta.url).href;export{e as i};
